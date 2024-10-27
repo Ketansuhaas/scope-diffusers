086c6a45f400dfc7246aceded0481bf98c7d3881
Ketan Saichandram <ketanss@scc-306.scc.bu.edu>
2024-10-27 10:14:26 -0400

    optimized temp decay, and removed temp dependence, stepsize 1,2,3 - good to go for evals

--- experiments.py
+++ experiments.py
@@ -50,11 +50,11 @@ if __name__ == "__main__":
             "DEVICE": "cuda",  # or "cpu"
             "seed": 42,
             "num_inference_steps": 50,
-            "step_sizes": [0.5,1,1.5,2,2.5,3,4], 
-            "temperatures": [0.5,1,10,100]#[1]#[0.5,1]#[0.05,0.1,0.3]   # doesn't matter for cslerp, and it is tau for emslerp
+            "step_sizes": [1,2,3], 
+            "temperatures": [1]   # doesn't matter for cslerp, and it is tau for emslerp
         }
 
-        with open('/projectnb/vkolagrp/ketanss/scope-diffusers/genai_prompts/scope_prompts_responses_part_rel.json', 'r') as file:
+        with open('/projectnb/vkolagrp/ketanss/scope-diffusers/genai_prompts/scope_prompts_responses_universal_new.json', 'r') as file:
             data = json.load(file)
 
         for exp_id in range(len(data)):
@@ -77,7 +77,7 @@ if __name__ == "__main__":
                 print(len(prompts))
             else:
                 print("No matching content found.")
-
+                
             config_overall["prompt_schedule"] = prompts
             exp_seed = SCoPE_Exp_overall(config_overall, args.exp_name, str(exp_id))
             exp_seed.run()
